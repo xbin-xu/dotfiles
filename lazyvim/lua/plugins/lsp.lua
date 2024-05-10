2523e1e441c684bc67cff11cef132fac09be4a09
Bin <1073788479@qq.com>
2024-05-10 17:35:50 +0800

    add keybingings  and  for lsp and use  to exec wt

--- lazyvim/lua/plugins/lsp.lua
+++ lazyvim/lua/plugins/lsp.lua
@@ -4,6 +4,12 @@ return {
   require("plugins.lang.markdown"),
   {
     "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- add a keymap
+      keys[#keys + 1] = { "<leader>ci", "<cmd>Telescope lsp_incoming_calls<cr>", "Incoming calls" }
+      keys[#keys + 1] = { "<leader>co", "<cmd>Telescope lsp_outgoing_calls<cr>", "Outgoing calls" }
+    end,
     opts = {
       servers = {
         vimls = {},
