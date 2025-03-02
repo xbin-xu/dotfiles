16b4e1e75851779e54fc5918548abb9d8c9af797
Bin <xbin.xu@qq.com>
2025-03-02 14:22:48 +0800

    feat(vscode): add some keybindings

--- lazyvim/lua/plugins/vscode.lua
+++ lazyvim/lua/plugins/vscode.lua
@@ -47,6 +47,7 @@ vim.api.nvim_create_autocmd("User", {
     vim.keymap.set("n", "]d", "<cmd>call VSCodeNotify('editor.action.marker.next')<cr>")
     vim.keymap.set("n", "[b", "<cmd>call VSCodeNotify('workbench.action.previousEditor')<cr>")
     vim.keymap.set("n", "]b", "<cmd>call VSCodeNotify('workbench.action.nextEditor')<cr>")
+    vim.keymap.set({ "n", "x" }, "gx", "<cmd>call VSCodeNotify('editor.action.openLink')<cr>")
   end,
 })
 
