a2f282ce4df530639ea41bd5284c542b7daa4d53
xbin <xbin.xu@qq.com>
2025-04-04 11:27:48 +0800

    feat(nvim): remove

--- nvim/init.lua
+++
@@ -1,5 +0,0 @@
-vim.loader.enable()
-require("config.autocmds")
-require("config.options")
-require("config.keymaps")
-require("config.lazy")
--- nvim/lua/plugins/lang/init.lua
+++
@@ -1,8 +0,0 @@
-return {
-    require("plugins.lang.clangd"),
-    require("plugins.lang.cmake"),
-    require("plugins.lang.markdown"),
-    require("plugins.lang.python"),
-    require("plugins.lang.json"),
-    require("plugins.lang.docker"),
-}
