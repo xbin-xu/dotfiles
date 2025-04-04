1ac477aabc582fbad1cf3e506b944a00176acbf8
xbin <xbin.xu@qq.com>
2025-04-04 17:32:41 +0800

    feat(yazi): enhance j/k, add mime plugin and remove unused plugins

--- yazi/plugins/arrow.yazi/main.lua
+++ yazi/plugins/arrow.yazi/main.lua
@@ -1,7 +1,8 @@
+--- @sync entry
 return {
-	entry = function(_, args)
+	entry = function(_, job)
 		local current = cx.active.current
-		local new = (current.cursor + args[1]) % #current.files
-		ya.manager_emit("arrow", { new - current.cursor })
+		local new = (current.cursor + job.args[1]) % #current.files
+		ya.mgr_emit("arrow", { new - current.cursor })
 	end,
 }
