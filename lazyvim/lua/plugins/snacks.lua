dd5bea5ba7e147a6ace8a3f68972736dfde52547
xbin <xbin.xu@qq.com>
2025-08-16 09:41:31 +0800

    feat(lazyvim): fix snacks explorer flash and enable recommended plugins

---
+++ lazyvim/lua/plugins/snacks.lua
@@ -0,0 +1,31 @@
+return {
+  -- lazy.nvim
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      explorer = {
+        -- your explorer configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+      picker = {
+        sources = {
+          explorer = {
+            -- your explorer picker configuration comes here
+            -- or leave it empty to use the default settings
+            watch = false,
+            win = {
+              list = {
+                keys = {
+                  -- ["o"] = "explorer_open", -- open with system application
+                  ["o"] = "confirm",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
