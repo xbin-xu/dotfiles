ad0b2d343bbc11feed2e0de47ece0e8ae8a05f24
xbin <xbin.xu@qq.com>
2025-08-26 07:58:17 +0800

    feat(lazyvim): add '<leader>bo'to close other unpinned buffers

--- lazyvim/lua/plugins/bufferline.lua
+++ lazyvim/lua/plugins/bufferline.lua
@@ -1,3 +1,43 @@
+local M = {}
+
+function M.get_first_unpinned_index()
+  local state = require("bufferline.state")
+  local groups = require("bufferline.groups")
+
+  for i, item in ipairs(state.components) do
+    if not groups._is_pinned(item:as_element()) then
+      return i
+    end
+  end
+  return 0
+end
+
+function M.close_other_unpinned()
+  local commands = require("bufferline.commands")
+  local state = require("bufferline.state")
+
+  local first_unppinned_index = M.get_first_unpinned_index()
+  if first_unppinned_index == 0 then
+    return
+  end
+  local index = commands.get_current_element_index(state)
+  if not index then
+    return
+  end
+
+  if index > first_unppinned_index then
+    commands.move_to(first_unppinned_index)
+    index = commands.get_current_element_index(state)
+    commands.close_in_direction("right")
+  elseif index < first_unppinned_index then
+    commands.go_to(first_unppinned_index - 1, true)
+    index = commands.get_current_element_index(state)
+    commands.close_in_direction("right")
+    commands.go_to(index, true)
+    index = commands.get_current_element_index(state)
+  end
+end
+
 return {
   {
     "akinsho/bufferline.nvim",
@@ -5,7 +45,9 @@ return {
       return {
         { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
         { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
-        { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
+        -- stylua: ignore
+        { "<leader>bo", function() M.close_other_unpinned() end, desc = "Delete other unpinned buffers" },
+        { "<leader>bO", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
         { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
         { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
         -- { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
