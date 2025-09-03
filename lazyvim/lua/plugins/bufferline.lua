33c61fa2dc20e3eb20145347d78a16c59cde28ef
xbin <xbin.xu@qq.com>
2025-09-03 16:22:57 +0800

    fix(lazyvim): fix bufferline colse_other_unpinned()

--- lazyvim/lua/plugins/bufferline.lua
+++ lazyvim/lua/plugins/bufferline.lua
@@ -15,17 +15,20 @@ end
 function M.close_other_unpinned()
   local commands = require("bufferline.commands")
   local state = require("bufferline.state")
+  -- local utils = require("bufferline.utils")
 
   local first_unppinned_index = M.get_first_unpinned_index()
+  -- utils.notify("first_unppinned_index: " .. first_unppinned_index, "info")
   if first_unppinned_index == 0 then
     return
   end
   local index = commands.get_current_element_index(state)
+  -- utils.notify("get_current_element_index: " .. index, "info")
   if not index then
     return
   end
 
-  if index > first_unppinned_index then
+  if index >= first_unppinned_index then
     commands.move_to(first_unppinned_index)
     index = commands.get_current_element_index(state)
     commands.close_in_direction("right")
