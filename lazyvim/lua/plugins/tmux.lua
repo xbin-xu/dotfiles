ac99f09442a6517fc1a22d2ca178e3561b315c29
xbin <xbin.xu@qq.com>
2026-05-04 09:17:46 +0800

    feat(lazyvim): add vim-tmux-navigator plugin

---
+++ lazyvim/lua/plugins/tmux.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    cond = function()
+      return vim.env.TMUX ~= nil
+    end,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+}
